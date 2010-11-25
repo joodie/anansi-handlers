43ddae35d90ae52b7a5d46dc0946bc4711bcc508
Joost Diepenmaat <joost@zeekat.nl>
2010-11-25 11:26:21 +0100

    update to latest compojure

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "An alternative syntax for specifying clout/compojure routes."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.4.1"]]
+                 [compojure "0.5.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
