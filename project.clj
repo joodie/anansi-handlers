c8437665327c7d41c129dbdc306fb3611f3cbf77
Joost Diepenmaat <joost@zeekat.nl>
2011-09-24 09:46:15 +0200

    removed swank-clojure deps

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject anansi-handlers "1.0.1"
+(defproject anansi-handlers "1.0.2"
   :description "An alternative syntax for specifying clout/compojure routes."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.5.3"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+                 [compojure "0.5.3"]])
